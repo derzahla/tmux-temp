ca219c7d973e09d3b854a53187f4a82af83ca5de
derzahl <46491701+derzahla@users.noreply.github.com>
2023-09-05 18:07:34 -0700

    Update temperature.sh

--- scripts/temperature.sh
+++ scripts/temperature.sh
@@ -4,7 +4,7 @@ SDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 source "$SDIR/helpers.sh"
 
 get_temperature() {
-    local thermal_zone=$(grep -l x86_pkg_temp /sys/class/thermal/thermal_zone*/type | head -n1)
+    local thermal_zone=$(grep -l core /sys/class/thermal/thermal_zone*/type | head -n1)
     if [ -f "$thermal_zone" ]; then
 		temperature=$(($(cat $(dirname $thermal_zone)/temp) / 1000))
 	else
